570462fc49a2c7caa1f5f7d49694706fedd8da4d
TomoCroissant <111544366+TomoCroissant@users.noreply.github.com>
2023-03-15 19:30:54 -0600

    Add files via upload

---
+++ SRC/Item.py
@@ -0,0 +1,13 @@
+class Item:
+  doesDamage = False
+  fireRange = 5
+
+  def __init__(self):
+    self.doesDamage = False
+    self.fireRange = 5
+
+  def itemAbility(self):
+        print(self.doesDamage)
+
+  def collisionDetection(self):
+        print(self.fireRange)
